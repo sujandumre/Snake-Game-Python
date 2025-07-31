932e34dfdd878d2eb106b40c35aab30773fa5ea4
Sujan Dumre <135045710+sujandumre@users.noreply.github.com>
2025-07-31 18:36:09 +0545

    snake.py

--- snake.py
+++ snake.py
@@ -2,30 +2,23 @@ import pygame
 import time
 import random
 import os
+import colorsys
 
 pygame.init()
 
-# Colors
-white = (255, 255, 255)
-yellow = (255, 255, 102)
-black = (0, 0, 0)
-red = (213, 50, 80)
-green = (0, 255, 0)
-blue = (50, 153, 213)
-
-# Display settings
+# Window settings
 dis_width = 600
 dis_height = 400
 dis = pygame.display.set_mode((dis_width, dis_height))
-pygame.display.set_caption('Snake Game')
+pygame.display.set_caption('🐍 Sweet Snake Game 🍭')
 
 clock = pygame.time.Clock()
 
 snake_block = 10
-snake_speed = 10
+snake_speed = 12
 
 # Fonts
-font_style = pygame.font.SysFont("bahnschrift", 25)
+font_style = pygame.font.SysFont("comicsansms", 25)
 score_font = pygame.font.SysFont("comicsansms", 30)
 
 # High score file
@@ -43,20 +36,41 @@ def save_high_score(score):
         f.write(str(score))
 
 def Your_score(score, high_score):
-    value = score_font.render(f"Score: {score}  High Score: {high_score}", True, yellow)
+    value = score_font.render(f"Score: {score}  High Score: {high_score}", True, (255,255,255))
     dis.blit(value, [10, 10])
 
-def our_snake(snake_block, snake_list):
-    for x in snake_list:
-        pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
+def draw_gradient_background():
+    for y in range(dis_height):
+        r = int(100 + (155 * y / dis_height))
+        g = int(100 + (80 * y / dis_height))
+        b = int(200 - (150 * y / dis_height))
+        pygame.draw.line(dis, (r, g, b), (0, y), (dis_width, y))
+
+def our_snake(snake_list):
+    for pos in snake_list:
+        pygame.draw.rect(dis, (0,0,0), [pos[0], pos[1], snake_block, snake_block])
 
 def message(msg, color):
     mesg = font_style.render(msg, True, color)
-    dis.blit(mesg, [dis_width / 6, dis_height / 3])
+    rect = mesg.get_rect(center=(dis_width/2, dis_height/2))
+    dis.blit(mesg, rect)
+
+def wait_for_key(msg="Press any key to start"):
+    draw_gradient_background()
+    message(msg, (255,255,255))
+    pygame.display.update()
+    waiting = True
+    while waiting:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                waiting = False
 
 def gameLoop():
     game_over = False
-    game_close = False
+    pause = False
 
     x1 = dis_width / 2
     y1 = dis_height / 2
@@ -76,51 +90,52 @@ def gameLoop():
     special_food = None
     special_food_value = 3
 
+    wait_for_key()
+
     while not game_over:
-
-        while game_close:
-            dis.fill(blue)
-            message("You Lost! Press C-Play Again or Q-Quit", red)
-            Your_score(Length_of_snake - 1, high_score)
+        while pause:
+            draw_gradient_background()
+            message("Paused - Press any key to resume", (255,255,255))
             pygame.display.update()
-
             for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_q:
-                        game_over = True
-                        game_close = False
-                    if event.key == pygame.K_c:
-                        gameLoop()
+                    pause = False
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_over = True
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT and x1_change == 0:
+                if x1_change == 0 and event.key == pygame.K_LEFT:
                     x1_change = -snake_block
                     y1_change = 0
-                elif event.key == pygame.K_RIGHT and x1_change == 0:
+                elif x1_change == 0 and event.key == pygame.K_RIGHT:
                     x1_change = snake_block
                     y1_change = 0
-                elif event.key == pygame.K_UP and y1_change == 0:
+                elif y1_change == 0 and event.key == pygame.K_UP:
                     y1_change = -snake_block
                     x1_change = 0
-                elif event.key == pygame.K_DOWN and y1_change == 0:
+                elif y1_change == 0 and event.key == pygame.K_DOWN:
                     y1_change = snake_block
                     x1_change = 0
+                elif event.key != pygame.K_LEFT and event.key != pygame.K_RIGHT and event.key != pygame.K_UP and event.key != pygame.K_DOWN:
+                    pause = True  # Press any other key to pause
 
         if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
-            game_close = True
+            game_over = True
 
         x1 += x1_change
         y1 += y1_change
-        dis.fill(blue)
+
+        draw_gradient_background()
 
         # Draw food
         if special_food:
-            pygame.draw.rect(dis, red, [special_food[0], special_food[1], snake_block, snake_block])
+            pygame.draw.rect(dis, (255,0,0), [special_food[0], special_food[1], snake_block, snake_block])
         else:
-            pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])
+            pygame.draw.rect(dis, (0,255,0), [foodx, foody, snake_block, snake_block])
 
         snake_Head = [x1, y1]
         snake_List.append(snake_Head)
@@ -129,14 +144,14 @@ def gameLoop():
 
         for x in snake_List[:-1]:
             if x == snake_Head:
-                game_close = True
+                game_over = True
 
         current_score = Length_of_snake - 1
         if current_score > high_score:
             high_score = current_score
             save_high_score(high_score)
 
-        our_snake(snake_block, snake_List)
+        our_snake(snake_List)
         Your_score(current_score, high_score)
 
         pygame.display.update()
@@ -163,7 +178,11 @@ def gameLoop():
 
         clock.tick(snake_speed)
 
-    pygame.quit()
-    quit()
+    draw_gradient_background()
+    message("Game Over! Press any key to play again", (255,0,0))
+    pygame.display.update()
+    
+    wait_for_key()
+    gameLoop()
 
 gameLoop()
